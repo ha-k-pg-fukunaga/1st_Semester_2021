dc0645109919fadd9084a1ed81ab2e4df96ab052
MSI\Yuk32 <Yuk321321@icloud.com>
2021-06-29 17:53:37 +0900

    fighter.hにプログラミング
    インクルードガードの実装、Fighterクラスの追加、及び内部処理

---
+++ GameProgramming/C_Practice10/02_Refactoring/fighter.h
@@ -0,0 +1,22 @@
+#ifndef FIGHTER_H
+#define FIGHTER_H
+
+class Fighter
+{
+public:
+	Fighter();
+	~Fighter();
+
+	char name[32];
+	int hp;
+	int str;
+	int mag;
+	int tec;
+	int spd;
+	int def;
+	int luc;
+	int mde;
+};
+
+
+#endif
